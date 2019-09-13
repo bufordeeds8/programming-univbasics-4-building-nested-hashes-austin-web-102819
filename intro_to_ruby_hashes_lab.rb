7ee984e8d1e54bfc945de79dd736cdc518770bf6
Buford Eeds <bufordeeds8@gmail.com>
2019-09-13 14:23:01 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,7 +22,7 @@ def monopoly_with_third_tier
 	{
 		railroads: {
 			rent_in_dollars: {owner1: "", owner2: "", owner3: "", owner4: ""},
-			names: "",
+			names: {key1: '', key2: '', key3: '', key4: ''},
 			pieces: 4
 		}
 	}
