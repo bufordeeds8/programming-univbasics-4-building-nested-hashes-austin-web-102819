1ceb7253e4a3afce2b7481349a0120edf3e74263
Buford Eeds <bufordeeds8@gmail.com>
2019-09-13 14:12:32 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -5,7 +5,7 @@ def base_hash
 end
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
-	
+
 def monopoly_with_second_tier
 	{
 		railroads: {}
